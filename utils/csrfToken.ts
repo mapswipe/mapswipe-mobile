f43112b8128df2410b886e419826570e62d24e80
AdityaKhatri <adityakhatri47@gmail.com>
2026-04-01 09:39:07 +0545

    fix: update cookie get function

--- utils/csrfToken.ts
+++ utils/csrfToken.ts
@@ -11,27 +11,21 @@ export async function fetchCsrfToken() {
             method: 'GET',
             credentials: 'include',
         });
-
-        const setCookie = response.headers.get('set-cookie');
-
-        if (setCookie) {
-            // Match MAPSWIPE-...-CSRFTOKEN
-            const match = setCookie.match(/(MAPSWIPE-\w+-CSRFTOKEN)=([^;]+)/);
-
-            if (match && match[2]) {
+        if (response.ok) {
+            const match = document.cookie.match(/(MAPSWIPE-[\w-]+-CSRFTOKEN)=([^;]+)/);
+            if (match) {
                 const cookieName = match[1];
                 const csrfToken = match[2];
                 await AsyncStorage.setItem(CSRF_COOKIE_NAME_KEY, cookieName);
                 await AsyncStorage.setItem(CSRF_KEY, csrfToken);
                 return csrfToken;
             }
-            return null;
         }
+        return null;
     } catch (err) {
         console.error('Failed to fetch CSRF token', err);
         return null;
     }
-    return null;
 }
 
 export async function getCsrfToken() {
