99fde5fd9e87c57adaff8578ebd7e0ee49f04ff5
AdityaKhatri <adityakhatri47@gmail.com>
2026-04-30 14:23:32 +0545

    feat: add apk signing in release workflow

---
+++ app.config.js
@@ -0,0 +1,10 @@
+const isStaging = process.env.APP_ENV === 'staging';
+
+const stagingConfig = require('./app.staging.json').expo;
+const prodConfig = require('./app.prod.json').expo;
+
+const config = isStaging ? stagingConfig : prodConfig;
+
+module.exports = {
+  expo: config,
+};
